a97de6daa776a60dd3f5511eb2499a917930e6db
John Storey <jstorey@wayfair.com>
2019-06-25 12:04:01 -0400

    gives code from redux-docs

--- src/App.js
+++ src/App.js
@@ -1,22 +1,16 @@
 import React from 'react';
-import logo from './logo.png';
+import Footer from './components/Footer'
+import AddTodo from './containers/AddTodo'
+import VisibleTodoList from './containers/VisibleTodoList'
+
 import './App.css';
 
 function App() {
   return (
-    <div>
-      <header className="App-header tac">
-        <img src={logo} alt="logo" className="h200" />
-        <p>
-          Not A Serious Application
-        </p>
-        <p className="text-small w500">
-          Of course this is a simplistic view of an application.
-          Many of the problems we encounter are when dealing with complex situations,
-          but my hope is that this will be a proving ground for simplifying complex things.
-          To be used when needed to express a new pattern or organization thought.
-        </p>
-      </header>
+    <div className='tac mt100'>
+      <AddTodo />
+      <VisibleTodoList />
+      <Footer />
     </div>
   );
 }
